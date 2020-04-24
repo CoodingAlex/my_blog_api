5c170bb42fe2810cc9e12d3ec49484d9ff708816
alejandroirz <alejandroirz@outlook.com>
2020-04-23 22:44:27 -0600

    Deploy

--- api/index.js
+++ api/index.js
@@ -1,24 +1,30 @@
 //config
 const config = require("../config");
+const serverless = require("serverless-http");
 //express
 const express = require("express");
 const App = express();
 const ErroHandler = require("./network/middlewares/ErrorHandler");
 const cors = require("cors");
 //Routes files
+const Router = express.Router();
+
 const posts = require("./components/posts/network");
 //middlewares
 App.use(express.json());
-
 App.use(cors());
 
 // Then use it before your routes are set up:
 //routes
-App.use("/posts", posts);
 
+Router.use("/posts", posts);
 //error middlewares
 App.use(ErroHandler);
+App.use("/.netlify/functions/server", Router); // path must route to lambda
 
 App.listen(config.api.port, () => {
   console.log(`Api listening on port ${config.api.port}`);
 });
+
+module.exports = App;
+module.exports.handler = serverless(App);
